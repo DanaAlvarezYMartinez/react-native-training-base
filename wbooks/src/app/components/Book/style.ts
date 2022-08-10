1d245af3fcb964d1c1bd00c8ab13100466438729
DanaAlvarezYMartinez <martinezdanaalvarez@gmail.com>
2022-08-10 17:34:46 -0300

    WIP: Created Book component

---
+++ wbooks/src/app/components/Book/style.ts
@@ -0,0 +1,36 @@
+import { StyleSheet } from "react-native";
+
+const style = StyleSheet.create({
+    bookContainer:{
+        display:'flex',
+        backgroundColor:'#fff',
+        flexDirection:'row',
+        borderRadius:8,
+        paddingVertical:15,
+        paddingHorizontal:30,
+        height:90,
+        alignItems:'center',
+        marginVertical:5,
+    },
+    bookInfoContainer:{
+        flex:1,
+        flexDirection:'column',
+        marginLeft:15,
+    },
+    bookTitle:{
+        fontSize:17,
+        color:'#4a4a4a',git
+        fontWeight:'700',
+    },
+    bookAuthor:{
+        fontSize:15,
+        color:'#4a4a4a',
+        fontWeight:'400',
+    },
+    img:{
+        width:40,
+        height:60,
+    }
+});
+
+export default style;
--- wbooks/src/app/index.tsx
+++ wbooks/src/app/index.tsx
@@ -10,90 +10,39 @@
  */
 
 import React from 'react';
-import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar } from 'react-native';
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions
-} from 'react-native/Libraries/NewAppScreen';
+import { SafeAreaView, StyleSheet, View, Text} from 'react-native';
+import Book from '@components/Book';
+import { BOOKS_MOCK } from '@constants/mockBooks';
+const book = BOOKS_MOCK[1];
 
 const App = () => {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
-          <Header />
-          <View style={styles.body}>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change this screen and then come back to
-                see your edits.
-              </Text>
+              <Text style={styles.sectionTitle}>Book Component Test</Text>
+              <Book author={book.author} title={book.title} imageUrl={book.imageUrl}/>
+              <Book author={book.author} title={book.title} imageUrl={book.imageUrl}/>
+              <Book author={book.author} title={book.title} imageUrl={book.imageUrl}/>
+
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>Read the docs to discover what to do next:</Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
       </SafeAreaView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter
-  },
-  engine: {
-    position: 'absolute',
-    right: 0
-  },
-  body: {
-    backgroundColor: Colors.white
-  },
   sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24
+    marginTop: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#000',
+    height:'100%',
   },
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
-    color: Colors.black
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark
-  },
-  highlight: {
-    fontWeight: '700'
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right'
+    color: '#fff',
+    marginBottom:15,
   }
 });
 
