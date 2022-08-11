391a050e7ffb151ef7e14e984e90bd1214fefc06
DanaAlvarezYMartinez <martinezdanaalvarez@gmail.com>
2022-08-11 18:11:24 -0300

    Created screen for book detail

---
+++ wbooks/src/app/screens/BookDetail/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import style from './style';
+
+const BookDetail = () => {
+  return (
+    <View style={style.bookDetailContainer}>
+      <Text>Hola :) </Text>
+    </View>
+  );
+};
+
+export default BookDetail;
---
+++ wbooks/src/app/screens/BookDetail/style.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+import { aliceBlue, black } from '@constants/colors';
+
+const style = StyleSheet.create({
+  bookDetailContainer: {
+    display: 'flex',
+    paddingVertical: 15,
+    alignItems: 'center',
+    paddingHorizontal: 15,
+    backgroundColor: aliceBlue,
+    height: '100%',
+    color: black
+  }
+});
+
+export default style;
